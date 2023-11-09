35957b951b4319580c6a757c851c7790803433bc
sanadoing <mountrive33@gmail.com>
2023-11-09 15:07:41 +0900

    [23.11.09] 대기업, it기업 문제 모음 풀이

--- 20920.py
+++ 20920.py
@@ -1,1 +1,14 @@
-N, M = int(input())
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+dic = dict()
+for _ in range(N):
+    word = input().rstrip()
+    if len(word) >= M:
+        if word not in dic:
+            dic[word] = 1
+        else:
+            dic[word] += 1
+result = sorted(dic.items(), key=lambda item: (-item[1], -len(item[0]), item[0]))
+for i in range(len(result)):
+    print(result[i][0])
