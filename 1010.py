11fc2f70dac59b936f608c2d5c84d4613202ffc9
sanadoing <mountrive33@gmail.com>
2023-11-25 21:17:57 +0900

    [23.11.25] dp 알고리즘 풀이

---
+++ 1010.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
